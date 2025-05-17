c60d5bc3e60e208455f8d0e0070e38e9f43d1681
Ilham593 <sosham127@gmail.com>
2025-05-17 04:18:30 +0000

    membuat endpoint get riwayat

--- controllers/riwayatController.js
+++ controllers/riwayatController.js
@@ -2,7 +2,7 @@ import Riwayat from "../models/Riwayat.js";
 
 export const simpanRiwayat = async (req, res) => {
   try {
-    const { penyakit, akurasi, saran, image} = req.body;
+    const { penyakit, akurasi, saran, image } = req.body;
 
     const userId = req.user.id;
 
@@ -17,9 +17,25 @@ export const simpanRiwayat = async (req, res) => {
     res.status(201).json({
       message: "Riwayat berhasil disimpan",
       riwayat: riwayatBaru,
-    })
-  }catch (error) {
+    });
+  } catch (error) {
     console.log("Error saving riwayat:", error);
     res.status(500).json({ message: "Gagal menyimpan riwayat" });
   }
-}
+};
+
+export const getRiwayat = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const riwayat = await Riwayat.find({ userId }).sort({ tanggal: -1 });
+
+    res.status(200).json({
+      message: "Riwayat berhasil diambil",
+      riwayat,
+    });
+  } catch (error) {
+    console.log("Error getting riwayat:", error);
+    res.status(500).json({ message: "Gagal mengambil riwayat" });
+  }
+};
--- routes/riwayat.js
+++ routes/riwayat.js
@@ -1,8 +1,9 @@
 import express from "express";
 import { verifyToken } from "../middlewares/authMiddleware.js";
-import { simpanRiwayat } from "../controllers/riwayatController.js";
+import { simpanRiwayat , getRiwayat } from "../controllers/riwayatController.js";
 
 const router = express.Router();
 
 router.post("/riwayat", verifyToken, simpanRiwayat);
+router.get("/riwayat", verifyToken, getRiwayat);
 export default router;
