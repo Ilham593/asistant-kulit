855f0ac78891605e73a90d8adf1d563358f19832
Ilham593 <sosham127@gmail.com>
2025-05-18 03:01:51 +0000

    fix router

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
+import swaagerDocs from "./swagger.js";
+
+// rooter
 import authrouter from "./routes/auth.js";
 import predictrouter from "./routes/predict.js";
 import riwayatrouter from "./routes/riwayat.js";
-import swaagerDocs from "./swagger.js";
-import swagger from "./swagger.js";
 // import artikelrouter from "./routes/artikel.js";
 // import statistikrouter from "./routes/statistik.js";
 dotenv.config();
@@ -18,11 +19,17 @@ app.use(express.json());
 
 // connet
 connectDB();
+
+
 app.use("/api/auth", authrouter);
 app.use("/api", predictrouter);
 app.use("/api", riwayatrouter);
 // app.use("/api", artikelrouter);
 // app.use("/api", statistikrouter);
+
+app.get("/", (req, res) => {
+  res.redirect("/api-docs");
+});
 
 const PORRT = process.env.PORT || 5000;
 swaagerDocs(app);
