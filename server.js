5cb4b374dd584812b902d626f3acc456ab59aa55
Ilham593 <sosham127@gmail.com>
2025-05-18 03:22:01 +0000

    fix

--- server.js
+++ server.js
@@ -2,39 +2,42 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
-import swaagerDocs from "./swagger.js";
+import swaggerDocs from "./swagger.js";
 
-// rooter
+// import router
 import authrouter from "./routes/auth.js";
 import predictrouter from "./routes/predict.js";
 import riwayatrouter from "./routes/riwayat.js";
 // import artikelrouter from "./routes/artikel.js";
 // import statistikrouter from "./routes/statistik.js";
+
 dotenv.config();
 const app = express();
+
+// connect ke database
+connectDB();
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-// connet
-connectDB();
-
-
+// routes utama
 app.use("/api/auth", authrouter);
 app.use("/api", predictrouter);
 app.use("/api", riwayatrouter);
 // app.use("/api", artikelrouter);
 // app.use("/api", statistikrouter);
 
+// Swagger setup 
+swaggerDocs(app);
+
+// redirect
 app.get("/", (req, res) => {
   res.redirect("/api-docs");
 });
 
-const PORRT = process.env.PORT || 5000;
-swaagerDocs(app);
-
-app.listen(PORRT, () => {
-  console.log(`Server running on port ${PORRT}`);
-  console.log("swagger docs running on http://localhost:5000/api-docs");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`✅ Server running on port ${PORT}`);
+  console.log(`📘 Swagger docs at http://localhost:${PORT}/api-docs`);
 });
