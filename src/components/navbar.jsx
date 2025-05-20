69b304d6f8eb8518850c8dcfb2cc6075350825d4
Achmad Rizaldi <rizaldi.ach03@gmail.com>
2025-05-20 15:38:58 +0700

    fix sidebar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -36,7 +36,7 @@ export default function Navbar() {
         </button>
       </div>
 
-      {open && <Sidebar onClose={()=>setOpen(false)} />}
+      {open && <Sidebar onClose={()=>setOpen(false)} navLinks={navLinks} />}
     </header>
   );
 }
